a356ca0adb127acd538cd49b4d29230db88eecc7
Cristian Romero <cristianrom66@gmail.com>
2025-04-28 16:52:41 -0500

    workout schema created

---
+++ src/workouts/schemas/workout.schema.ts
@@ -0,0 +1,27 @@
+/** npm imports */
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
+import { Document } from 'mongoose'
+
+@Schema()
+export class Workout extends Document {
+  @Prop({ required: true })
+  name: string
+
+  @Prop({ required: true })
+  mode: string
+
+  @Prop({ required: true, type: [String] })
+  equipment: string[]
+
+  @Prop({ required: true, type: [String] })
+  exercises: string[]
+
+  @Prop({ required: true, type: [String] })
+  trainerTips: string[]
+
+  @Prop({ default: Date.now })
+  createdAt: string
+
+  @Prop({ default: Date.now })
+  updatedAt: string
+}
